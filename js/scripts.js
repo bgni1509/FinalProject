c66ad2f3a56d41164a5af7520012de183a8e35e0
bgni1509 <bgni1509@gmail.com>
2024-05-17 16:10:31 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    // autoplay:true,
+    // autoplayTimeout:1000,
+    loop: true,
+    nav: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1200:{
+            items:4
+        }
+        
+    }
+});
